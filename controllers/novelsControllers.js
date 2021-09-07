9520106c915df4d0e43be50931e80b4595c02ab7
katskurka <kati.skurka@gmail.com>
2021-09-07 14:39:41 -0400

    added controllers and required them in index.js

---
+++ controllers/novelsControllers.js
@@ -0,0 +1,26 @@
+const models = require('..models')
+
+const getAllNovels = async (request, response) => {
+  const novels = await models.Novels.findAll({
+    include: [{ model: models.Authors }, { model: models.Genres }]
+  })
+
+  return response.send(novels)
+}
+
+const getAllByNovelId = async (request, response) => {
+  try {
+    const { id } = request.params
+
+    const novel = await models.Novels.findOne({
+      where: { id },
+      include: [{ model: models.Authors }, { model: models.Genres }]
+    })
+
+    return novel ? response.send(novel) : response.sendStatus(404)
+  } catch (error) {
+    return response.status(500).send('no novels found')
+  }
+}
+
+module.exports = { getAllNovels, getAllByNovelId }
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const { getAllAuthors, getAllByAuthorId } = require('./controllers/authorsControllers')
+const { getAllGenres, getAllByGenreId } = require('./controllers/genresControllers')
+const { getAllNovels, getAllByNovelId } = require('./controllers/novelsControllers')
 
 const app = express()
 
@@ -6,15 +9,15 @@ app.use(express.json())
 
 app.get('/authors', getAllAuthors) // get all authors from DB
 
-app.get('/authors/:id', getAuthorsById) // gets all authors with their novels and those novels genres by author's id
+app.get('/authors/:id', getAllByAuthorId) // gets all authors with their novels and those novels genres by author's id
 
 app.get('/genres', getAllGenres) // gets all genres from DB
 
-app.get('/genres/:id', getGenresByID) // get a genre with all novels in that genre and those novels author by genre's id
+app.get('/genres/:id', getAllByGenreId) // get a genre with all novels in that genre and those novels author by genre's id
 
 app.get('/novels', getAllNovels) // get all novels with their authors and genres
 
-app.get('/novels/:id', getNovelById) // get a novel with its author and genres by the novel's id
+app.get('/novels/:id', getAllByNovelId) // get a novel with its author and genres by the novel's id
 
 app.listen(3010, () => {
   console.log('ET phone home') // eslint-disable-line no-console
